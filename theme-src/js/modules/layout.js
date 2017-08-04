8d46efa5cf5be44383186b0d54c3b24f4e267d1c
Stephen <stephenritchie0@gmail.com>
2017-08-04 10:50:05 +1000

    add some basic example documentation comments

--- theme-src/js/modules/layout.js
+++ theme-src/js/modules/layout.js
@@ -2,12 +2,26 @@
  * This is the main Layout
  */
 class Layout {
+	/**
+	 * Constructor for layout class
+	 * @param {string[]} this.consoleLogStatement is a string of text to be console.logged to verify that the js is working correctly
+	 */
+	constructor() {
+		this.consoleLogStatement = 'Yup, it works!'
+	}
+
+	/**
+	 * Initial method that runs when the class is first instantiated
+	 */
 	init() {
 		this.consoleLogTest();
 	}
 
+	/**
+	 * consoleLogTest is a method that simple prints out a variable to the console
+	 */
 	consoleLogTest() {
-		console.log('Yup, it works!');
+		console.log(this.consoleLogStatement);
 	}
 }
 
